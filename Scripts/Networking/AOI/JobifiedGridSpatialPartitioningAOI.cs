6a69977a571bad24287c0d49f12429a867b9aaa7
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2026-04-10 00:41:24 +0700

    feat: update AOI, make it really multithreading

--- Scripts/Networking/AOI/JobifiedGridSpatialPartitioningAOI.cs
+++ Scripts/Networking/AOI/JobifiedGridSpatialPartitioningAOI.cs
@@ -22,9 +22,50 @@ namespace MultiplayerARPG
         private JobifiedGridSpatialPartitioningSystem _system;
         private float _updateCountDown;
         private Bounds _bounds;
-        private List<SpatialObject> _spatialObjects = new List<SpatialObject>();
         private Dictionary<uint, HashSet<uint>> _playerSubscribings = new Dictionary<uint, HashSet<uint>>();
         private HashSet<uint> _alwaysVisibleObjects = new HashSet<uint>();
+        private Queue<NativeList<SpatialObject>> _queryQueue = new Queue<NativeList<SpatialObject>>();
+        private Dictionary<uint, NativeList<SpatialObject>> _queryingPlayerSubscribings = new Dictionary<uint, NativeList<SpatialObject>>();
+        private Dictionary<uint, NativeList<SpatialObject>> _queryingComponentSubscribings = new Dictionary<uint, NativeList<SpatialObject>>();
+        private bool _isQuerying = false;
+
+        public NativeList<SpatialObject> ReserveResultList()
+        {
+            if (_queryQueue.Count > 0)
+            {
+                return _queryQueue.Dequeue();
+            }
+            else
+            {
+                return new NativeList<SpatialObject>(1024, Allocator.Persistent);
+            }
+        }
+
+        public void ReturnResultList(NativeList<SpatialObject> resultList)
+        {
+            resultList.Clear();
+            _queryQueue.Enqueue(resultList);
+        }
+
+        public void DisposeResultLists()
+        {
+            foreach (var resultList in _queryingPlayerSubscribings)
+            {
+                if (resultList.Value.IsCreated)
+                    resultList.Value.Dispose();
+            }
+            foreach (var resultList in _queryingComponentSubscribings)
+            {
+                if (resultList.Value.IsCreated)
+                    resultList.Value.Dispose();
+            }
+            while (_queryQueue.Count > 0)
+            {
+                NativeList<SpatialObject> resultList = _queryQueue.Dequeue();
+                if (resultList.IsCreated)
+                    resultList.Dispose();
+            }
+        }
 
         private void OnDrawGizmosSelected()
         {
@@ -37,6 +78,7 @@ namespace MultiplayerARPG
         private void OnDestroy()
         {
             _system = null;
+            DisposeResultLists();
         }
 
         public override void Setup(LiteNetLibGameManager manager)
@@ -136,7 +178,7 @@ namespace MultiplayerARPG
 
             using (s_UpdateProfilerMarker.Auto())
             {
-                _spatialObjects.Clear();
+                _system.ClearObjects();
                 var players = Manager.GetPlayers();
                 while (players.MoveNext())
                 {
@@ -150,32 +192,16 @@ namespace MultiplayerARPG
                     while (playerObjs.MoveNext())
                     {
                         LiteNetLibIdentity playerObj = playerObjs.Current.Value;
-                        _spatialObjects.Add(new SpatialObject()
+                        _system.AddObjectToGrid(new SpatialObject()
                         {
                             objectId = playerObj.ObjectId,
                             position = playerObj.transform.position,
                         });
                     }
-
                 }
-                _system.UpdateGrid(_spatialObjects);
-            }
-        }
+                _system.UpdateGrid();
 
-        private void LateUpdate()
-        {
-            if (_system == null)
-                return;
-
-            if (!_system.Complete())
-                return;
-
-            using (s_CompleteProfilerMarker.Auto())
-            {
                 _alwaysVisibleObjects.Clear();
-                NativeList<SpatialObject> queryResult;
-                HashSet<uint> subscribings;
-                LiteNetLibIdentity foundPlayerObject;
                 var spawnedObjects = Manager.Assets.GetSpawnedObjects();
                 while (spawnedObjects.MoveNext())
                 {
@@ -187,7 +213,56 @@ namespace MultiplayerARPG
                         _alwaysVisibleObjects.Add(spawnedObject.ObjectId);
                         continue;
                     }
-                    queryResult = _system.QuerySphere(spawnedObject.transform.position, GetVisibleRange(spawnedObject));
+                    NativeList<SpatialObject> nativeQueryResult = ReserveResultList();
+                    _queryingPlayerSubscribings[spawnedObject.ObjectId] = nativeQueryResult;
+                    _system.QuerySphere(spawnedObject.transform.position, GetVisibleRange(spawnedObject), nativeQueryResult);
+                }
+
+                var components = SpatialObjectContainer.GetEnumerator();
+                while (components.MoveNext())
+                {
+                    ISpatialObjectComponent component = components.Current.Value;
+                    if (component == null)
+                        continue;
+                    component.ClearSubscribers();
+                    if (!component.SpatialObjectEnabled)
+                        continue;
+                    NativeList<SpatialObject> nativeQueryResult = ReserveResultList();
+                    _queryingComponentSubscribings[component.SpatialObjectId] = nativeQueryResult;
+                    switch (component.SpatialObjectShape)
+                    {
+                        case SpatialObjectShape.Box:
+                            _system.QueryBox(component.SpatialObjectPosition, component.SpatialObjectExtents, nativeQueryResult);
+                            break;
+                        default:
+                            _system.QuerySphere(component.SpatialObjectPosition, component.SpatialObjectRadius, nativeQueryResult);
+                            break;
+                    }
+                }
+
+                _isQuerying = true;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (!_isQuerying)
+                return;
+
+            _isQuerying = false;
+
+            using (s_CompleteProfilerMarker.Auto())
+            {
+                _system.Complete();
+
+                NativeList<SpatialObject> queryResult;
+                HashSet<uint> subscribings;
+                LiteNetLibIdentity foundPlayerObject;
+                foreach (var playerQueryKvp in _queryingPlayerSubscribings)
+                {
+                    if (!Manager.Assets.TryGetSpawnedObject(playerQueryKvp.Key, out LiteNetLibIdentity spawnedObject))
+                        continue;
+                    queryResult = playerQueryKvp.Value;
                     for (int i = 0; i < queryResult.Length; ++i)
                     {
                         uint contactedObjectId = queryResult[i].objectId;
@@ -204,35 +279,24 @@ namespace MultiplayerARPG
                         subscribings.Add(spawnedObject.ObjectId);
                         _playerSubscribings[contactedObjectId] = subscribings;
                     }
-                    queryResult.Dispose();
+                    ReturnResultList(queryResult);
                 }
+                _queryingPlayerSubscribings.Clear();
 
-                var components = SpatialObjectContainer.GetEnumerator();
-                while (components.MoveNext())
+                foreach (var componentQueryKvp in _queryingComponentSubscribings)
                 {
-                    ISpatialObjectComponent component = components.Current.Value;
-                    if (component == null)
+                    if (!SpatialObjectContainer.TryGet(componentQueryKvp.Key, out ISpatialObjectComponent component))
                         continue;
-                    component.ClearSubscribers();
-                    if (!component.SpatialObjectEnabled)
-                        continue;
-                    switch (component.SpatialObjectShape)
-                    {
-                        case SpatialObjectShape.Box:
-                            queryResult = _system.QueryBox(component.SpatialObjectPosition, component.SpatialObjectExtents);
-                            break;
-                        default:
-                            queryResult = _system.QuerySphere(component.SpatialObjectPosition, component.SpatialObjectRadius);
-                            break;
-                    }
+                    queryResult = componentQueryKvp.Value;
                     for (int i = 0; i < queryResult.Length; ++i)
                     {
                         uint contactedObjectId = queryResult[i].objectId;
                         if (Manager.Assets.TryGetSpawnedObject(contactedObjectId, out foundPlayerObject))
                             component.AddSubscriber(foundPlayerObject.ObjectId);
                     }
-                    queryResult.Dispose();
+                    ReturnResultList(queryResult);
                 }
+                _queryingComponentSubscribings.Clear();
 
                 var players = Manager.GetPlayers();
                 while (players.MoveNext())
